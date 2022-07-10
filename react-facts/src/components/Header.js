7cbf3da960965fd0ccf17d4cd0235059f04ec4db
zafrantsany <zafrantsany30@gmail.com>
2022-07-10 10:31:15 +0700

    update

--- react-facts/src/components/Header.js
+++ react-facts/src/components/Header.js
@@ -5,15 +5,9 @@ export default function Header() {
   return (
     <header>
       <nav>
-        <div className="logo">
-          <img classname="icon" src={Icon} alt="React Logo" />
-          <div className="icon-title">
-            <h2>ReactFacts</h2>
-          </div>
-        </div>
-        <div className="title">
-          <h4>React Course - Project 1</h4>
-        </div>
+        <img classname="nav--icon" src={Icon} alt="React Logo" />
+        <h2 className="nav--logo_text">ReactFacts</h2>
+        <h4 className="nav--title">React Course - Project 1</h4>
       </nav>
     </header>
   );
