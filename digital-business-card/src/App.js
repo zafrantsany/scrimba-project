dfbf9dbe14bd1bde8e0b004eac9fefcb76dafc85
zafrantsany <zafrantsany30@gmail.com>
2022-07-12 07:40:13 +0700

    adding digital-business-card

---
+++ digital-business-card/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import Main from "./components/main";
+import Footer from "./components/footer";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Main />
+      <Footer />
+    </div>
+  );
+}
